09cc849a00665b859aea60d9890e8dd4b6bfe06c
MOHAMMAD AMMAR AZAM <93309473+ammar9525@users.noreply.github.com>
2024-08-25 19:18:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ app.use("/api/v1",auth);
 app.use("/api/v2",list);
 
 
-app.listen(1000, () => {
-    console.log("Server Started")
-});
-
 app.listen(process.env.PORT,() => {
     console.log(`Server Started at port ${process.env.PORT}`);
 })
